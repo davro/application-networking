f2c0aa4369b359d3fad6a61af7b2388f3e5b1a49
David Stevens <dstevens@NBV-PC-031.(none)>
2012-11-06 11:29:38 +0000

    Testing

--- Networking.php
+++ Networking.php
@@ -116,7 +116,7 @@ class Networking extends Actions {
 	 * 
 	 * Methods prefixed with cron will be executed by the framemwork when the 
 	 * system cron is run, annotation for running services.
-	 * Old example /usr/bin/git fetch --all && /usr/bin/git reset --hard origin/master
+	 * Broken example /usr/bin/git fetch --all && /usr/bin/git reset --hard origin/master
 	 * 
 	 * @service system
      * @command cd /home/davro/workspace/project-davro.net/applications/Networking && /usr/bin/git pull 
